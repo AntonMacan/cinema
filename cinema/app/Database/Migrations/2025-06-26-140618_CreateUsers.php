f41bea9c3204746e62ad25f6bbe5b0c92f3030d1
Anton Macan <m320848@alunos.ipb.pt>
2025-06-26 16:15:24 +0200

    Added Users

---
+++ cinema/app/Database/Migrations/2025-06-26-140618_CreateUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsers extends Migration
+{
+    public function up()
+{
+    $this->forge->addField([
+        'id' => [
+            'type' => 'INT',
+            'unsigned' => true,
+            'auto_increment' => true
+        ],
+        'nome' => ['type' => 'VARCHAR', 'constraint' => 100],
+        'email' => ['type' => 'VARCHAR', 'constraint' => 150],
+        'password' => ['type' => 'VARCHAR', 'constraint' => 255],
+        'codice_fiscale' => ['type' => 'VARCHAR', 'constraint' => 16],
+        'data_nascita' => ['type' => 'DATE'],
+        'created_at' => ['type' => 'TIMESTAMP', 'null' => true],
+        'updated_at' => ['type' => 'TIMESTAMP', 'null' => true],
+    ]);
+    $this->forge->addKey('id', true);
+    $this->forge->createTable('users');
+}
+
+public function down()
+{
+    $this->forge->dropTable('users');
+}
+
+}
